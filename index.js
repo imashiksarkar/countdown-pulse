8cd82dd47ae93f16403641e3f2e8163ba8d9f9e8
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2022-03-09 20:47:26 +0600

    js dock added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 class Countdown {
+  /**
+   * @param {object} cdnTime
+   */
   constructor(cdnTime) {
+    // gets the validated time
     const validatedTime = this.valitateTime(cdnTime);
     // initiated time for the countdown to close
     this.initiatedCountdown = new Date(validatedTime).getTime();
@@ -11,6 +15,10 @@ class Countdown {
       seconds: 0,
     };
   }
+  /**
+   *calculates current time
+   * @returns {{days:number, hours:number, minutes:number, seconds:number}}
+   */
   calculateTime() {
     // current time
     const currentTime = new Date().getTime();
@@ -30,7 +38,9 @@ class Countdown {
 
     return { days, hours, minutes, seconds };
   }
-
+  /**
+   * runs the function every second and send the value to particullar function
+   */
   mainTimeOut = setInterval(() => {
     const { days, hours, minutes, seconds } = this.calculateTime();
     if (this.calcSeconds && this.states.seconds !== seconds) {
@@ -54,7 +64,11 @@ class Countdown {
   stop() {
     clearInterval(this.mainTimeOut);
   }
-  // this function validates the user submitted time
+  /**
+   *this function validates the user submitted time
+   * @param {string} time
+   * @returns {string}
+   */
   valitateTime(time) {
     // throwing error if the date is not valid
     if (!time.date || time.date > 31 || typeof time.date !== "string")
@@ -85,7 +99,7 @@ class Countdown {
       throw new Error(
         "Provide a valide month name [example: 'february' or 'February']"
       );
-    const month = this.toUpperCase(time.month).slice(0, 3);
+    const month = this.toCapital(time.month).slice(0, 3);
 
     // throwing error if the year is not valid
     if (!time.year || typeof time.year !== "string")
@@ -109,11 +123,18 @@ class Countdown {
 
     return `${month} ${date}, ${year} ${hour}:${minute}:${second}`;
   }
-  // this method helps converting string to upper case
-  toUpperCase(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  /**
+   *this method helps converting string to upper case
+   * @param {string} string
+   * @returns {string}
+   */
+  toCapital = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 }
+/**
+ *
+ * @param {{date:string,month:string,year:string,hour:string,minute:string,second:string}} initatedTime
+ * @returns {{}}
+ */
 const countdownTimer = (initatedTime) => {
   return new Countdown(initatedTime);
 };
